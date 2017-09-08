8436abffcdf64a7d072e61edcf8f833457426238
cpan001 <github email address>
2017-09-08 11:11:22 -0400

    Done.

--- author.rb
+++ author.rb
@@ -1,1 +1,39 @@
+class Author
+  attr_accessor :books
+  attr_reader :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @books = []
+    @@all << self
+  end
+
+  def write_book(title, word_count)
+    book = Book.new(title, word_count)
+    self.books << book
+    book.author = self
+    book
+  end
+
+  def total_words
+    self.books.inject(0) {|memo, book| memo + book.word_count}
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  def self.most_words
+    # following method returns single instance of author with most toal words but not account for if 2 authors had same most words
+    # self.all.max_by do |author|
+    #   author.total_words
+    # end
+
+    # following methods returns array of author instance of author(s) that wrote most words
+    self.all.group_by {|author| author.total_words}.max[1]
+  end
+
+end
--- book.rb
+++ book.rb
@@ -1,1 +1,16 @@
+class Book
+  attr_accessor :author, :word_count
+  attr_reader :title
 
+  def initialize(title, word_count)
+    @title = title
+    @word_count = word_count
+  end
+
+  def self.all
+    Author.all.collect do |author|
+      author.books
+    end.flatten
+  end
+
+end
